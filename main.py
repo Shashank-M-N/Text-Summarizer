6b002610df3144a3ecfff1707bd852282ca5a0f0
Shashank M N <mnshashank432@gmail.com>
2024-12-27 15:02:46 +0530

    Model evaluation completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 from filelock import FileLock
 
@@ -49,9 +50,11 @@ with FileLock(LOCK_FILE):
             logger.exception(e)
             raise e
 
-        STAGE_NAME = "Test"
+        STAGE_NAME = "Model Evaluation stage"
         try:
             logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+            model_evaluation = ModelEvaluationTrainingPipeline()
+            model_evaluation.main()
             logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n")
         except Exception as e:
             logger.exception(e)
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-@dataclass(frozen=True)
+@dataclass(frozen = True)
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
     
-@dataclass(frozen=True)
+@dataclass(frozen = True)
 class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
@@ -38,3 +38,11 @@ class ModelTrainerConfig:
     save_steps: int
     save_total_limit: int
     load_best_model_at_end: bool
+    
+@dataclass(frozen = True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
